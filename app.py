f57dff2d5589d5f26e869524a0d19ea3e762bb27
Pramath Haritz <101699856+pramath-haritz@users.noreply.github.com>
2022-11-12 12:17:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
 app = Flask(__name__)
-
-from knn_parkinsons_diagnosis import call
-from knn_parkinsons_diagnosis import call1
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
-    call()
-    call1()
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    intx = y_test
+ 
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    prediction = model.predict(data)
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
